859734ae07c915fe6d5cf067a732169fc92d7f99
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-09 14:10:17 +0100

    Adding bash

--- fit_iris_data.py
+++ fit_iris_data.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # %% [markdown]
 # **Notebook to fit IRIS data using multiple cores**
 
-##!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 # %%
 
 import glob
