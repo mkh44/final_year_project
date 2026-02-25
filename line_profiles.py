dce8d9b86c7ae5f7553e9f2a275e1a5579205ab6
mkh44 <molly.hurley24@mail.dcu.ie>
2026-02-25 13:23:51 +0000

    made pixels separate

--- line_profiles.py
+++ line_profiles.py
@@ -40,6 +40,13 @@ nw, ny, nt = cube.shape
 # Compute Doppler velocity using centroid
 # Choose rest wavelength (Si IV for now)
 rest_wavlen = 1393.27 # Angstrom
+window = 1
+wl_min = rest_wavlen - window/2
+wl_max = rest_wavlen + window/2
+
+# Refine wavelength
+wl_mask = (wavelength >= wl_min) & (wavelength <= wl_max)
+wl_short = wavelength[wl_mask]
 
 # Compute centroid
 velocity = np.zeros((cube.shape[1], cube.shape[2]))
@@ -62,7 +69,7 @@ for y in range(cube.shape[1]):
 flat_indices = np.argsort(velocity.flatten())[::-1]
 
 # Get top N redshift pixels
-N = 10
+N = 2
 
 indices = np.unravel_index(flat_indices[:N], velocity.shape)
 
@@ -70,16 +77,13 @@ y_indices = indices[0]
 t_indices = indices[1]
 
 # Plot spectral profiles
-plt.figure(figsize=(8,6))
-
-for y, t in zip(y_indices, t_indices):
+for i, (y, t) in enumerate((y_indices, t_indices)):
     spectrum = cube[:, y, t]
-
-    plt.plot(wavelength, spectrum,
+    plt.figure(figsize=(8, 6))
+    plt.plot(wl_short, spectrum[wl_mask],
              label=f"y={y}, t={t}, v={velocity[y, t]:.1f} km/s")
     plt.xlabel("Wavelength (Å)")
     plt.ylabel("Intensity")
     plt.legend()
-    plt.title("Fastest redshifted pixels")
 
     plt.show()
