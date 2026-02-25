0fffeeb3dba459b5fd6104e13222cddf768d6a37
mkh44 <molly.hurley24@mail.dcu.ie>
2026-02-25 13:33:07 +0000

    Intensity is negative also I think I'm not looking at the right lines

--- line_profiles.py
+++ line_profiles.py
@@ -40,7 +40,7 @@ nw, ny, nt = cube.shape
 # Compute Doppler velocity using centroid
 # Choose rest wavelength (Si IV for now)
 rest_wavlen = 1393.27 # Angstrom
-window = 1
+window = 3 # Angstrom
 wl_min = rest_wavlen - window/2
 wl_max = rest_wavlen + window/2
 
@@ -65,25 +65,14 @@ for y in range(cube.shape[1]):
         velocity[y, t] = c * (centroid - rest_wavlen) / rest_wavlen
 
 
-# Find fastest redshift pixels
-flat_indices = np.argsort(velocity.flatten())[::-1]
-
-# Get top N redshift pixels
-N = 2
-
-indices = np.unravel_index(flat_indices[:N], velocity.shape)
-
-y_indices = indices[0]
-t_indices = indices[1]
 
 # Plot spectral profiles
-for i, (y, t) in enumerate((y_indices, t_indices)):
-    spectrum = cube[:, y, t]
-    plt.figure(figsize=(8, 6))
-    plt.plot(wl_short, spectrum[wl_mask],
-             label=f"y={y}, t={t}, v={velocity[y, t]:.1f} km/s")
-    plt.xlabel("Wavelength (Å)")
-    plt.ylabel("Intensity")
-    plt.legend()
+plt.figure(figsize=(8, 6))
+spectrum_short = cube[:, y, t][wl_mask]
+plt.plot(wl_short, spectrum_short,
+         label=f"y={y}, t={t}, v={velocity[y, t]:.1f} km/s")
+plt.xlabel("Wavelength (Å)")
+plt.ylabel("Intensity")
+plt.legend()
 
-    plt.show()
+plt.show()
