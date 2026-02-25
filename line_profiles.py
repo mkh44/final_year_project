912048dec0f23774a4b6f2cf3e16b85240e36c6b
mkh44 <molly.hurley24@mail.dcu.ie>
2026-02-25 12:23:34 +0000

    changed output location

--- line_profiles.py
+++ line_profiles.py
@@ -7,29 +7,17 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from astropy.io import fits
 from fit_iris_lines import fit_raster
-from zoomed_plots import output_loc
 
-
-def plot_pixel_spectrum(file, iris_window, xpix, ypix):
-    # Load raster
-    a = fit_raster(file, iris_window, fulldisk=False)
-    data, main_header, header, wavelength = a.open_iris_file(iris_window)
-
-    # Extract spectrum at pixel
-    spectrum = data[ypix, xpix, :]
-
-    # Plot
-    plt.figure(figsize=(6, 4))
-    plt.plot(wavelength, spectrum, color='black')
-    plt.xlabel("Wavelength (Å)")
-    plt.ylabel("Intensity")
-    plt.title(f"{iris_window} | Pixel (x={xpix}, y={ypix})")
-    plt.grid(alpha=0.3)
-    plt.tight_layout()
-    plt.savefig(os.path.join(output_loc, ) + iris_window.replace(' ', '_') + .png")
-    plt.close(fig)
-
+# Define event and output location
 event = '20230503_072923'
 output_loc = r"C:\Users\molly\OneDrive\OneDrive - Dublin City University personal\PHA4\Final_Year_Project\outputs"
-plot_pixel_spectrum(file, "Si IV 1403", xpix=100, ypix=50)
+
+# Load FITS file
+fits_file = glob.glob(os.path.join(output_loc, "*.fits"))[0]
+
+# Compute Doppler shifts from spectral cube
+hdul = fits.open(fits_file)
+
+
