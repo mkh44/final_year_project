a004a98aadcfc745a3ab11fc6b4b2249229555a9
mkh44 <molly.hurley24@mail.dcu.ie>
2026-03-04 16:43:20 +0000

    added wavelength plot and save to local

--- line_profiles.py
+++ line_profiles.py
@@ -20,15 +20,16 @@ from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 
 
 event = '20230503_072923'
-output_loc = r"C:\Users\molly\OneDrive\OneDrive - Dublin City University personal\PHA4\Final_Year_Project\outputs"
+output_loc = r"C:\Users\molly\OneDrive\OneDrive - Dublin City University personal\PHA4\Final_Year_Project\outputs\line_profiles"
+input_loc = r"C:\Users\molly\OneDrive\OneDrive - Dublin City University personal\PHA4\Final_Year_Project\outputs"
 
 # Load FITS file
-fits_file = glob.glob(os.path.join(output_loc, "iris_l2_20230503_072923_4204700135_raster_t000_r00000.fits"))[0]
+fits_file = glob.glob(os.path.join(input_loc, "iris_l2_20230503_072923_4204700135_raster_t000_r00000.fits"))[0]
 
 lines = [5, 1, 9]
-time_x = 7000
+time_x = 7100
 height = 100
-y_lim = 210
+y_lim = 75
 x_lim = 200
 
 hdul = fits.open(fits_file)
@@ -78,9 +79,8 @@ def line_profile(lines, time_x):
     lambda_mg = 2795.5
     mg_v_dopp = ((mg_wavelength - lambda_mg) / lambda_mg) * (c / 1e3)
 
-    # Plotting
-    plt.figure(figsize=(8, 6))
-    fig, ax = plt.subplots(3, 1)
+    # Plotting Doppler velocity
+    fig, ax = plt.subplots(3, 1, sharex = True)
     ax[0].plot(si_v_dopp, si_data_arr[time_x, height], color='k')
     ax[0].set_xlabel(' ')
     ax[0].set_ylim(0, y_lim)
@@ -88,7 +88,6 @@ def line_profile(lines, time_x):
     ax0.set_yticks([])
     ax0.set_yticklabels([])
     ax0.set_ylabel(f'{si_title}')
-    si_vmax = np.max(np.abs(si_v_dopp))
     ax[0].set_xlim(-x_lim, x_lim)
     ax[0].xaxis.set_minor_locator(MultipleLocator(10))
 
@@ -100,7 +99,6 @@ def line_profile(lines, time_x):
     ax1.set_yticks([])
     ax1.set_yticklabels([])
     ax1.set_ylabel(f'{cii_title}')
-    cii_vmax = np.max(np.abs(cii_v_dopp))
     ax[1].set_xlim(-x_lim, x_lim)
     ax[1].xaxis.set_minor_locator(MultipleLocator(10))
 
@@ -112,13 +110,51 @@ def line_profile(lines, time_x):
     ax2.set_yticks([])
     ax2.set_yticklabels([])
     ax2.set_ylabel(f'{mg_title}')
-    mg_vmax = np.max(np.abs(mg_v_dopp))
     ax[2].set_xlim(-x_lim, x_lim)
     ax[2].xaxis.set_minor_locator(MultipleLocator(10))
 
     plt.suptitle(f'Time: {time_x} s')
+    save_path = os.path.join(output_loc, f"Doppler_Profiles_{time_x}s.png")
+    plt.savefig(save_path, bbox_inches='tight')
     plt.show()
+    plt.close(fig)
 
+    # Plotting wavelength
+    fig, ax = plt.subplots(3, 1)
+    ax[0].plot(si_wavelength, si_data_arr[time_x, height], color='k')
+    ax[0].set_xlabel(' ')
+    ax[0].set_ylim(0, y_lim)
+    ax0 = ax[0].twinx()
+    ax0.set_yticks([])
+    ax0.set_yticklabels([])
+    ax0.set_ylabel(f'{si_title}')
+    ax[0].xaxis.set_minor_locator(MultipleLocator(10))
+
+    ax[1].plot(cii_wavelength, cii_data_arr[time_x, height], color='k')
+    ax[1].set_xlabel(' ')
+    ax[1].set_ylabel("Intensity")
+    ax[1].set_ylim(0, y_lim)
+    ax1 = ax[1].twinx()
+    ax1.set_yticks([])
+    ax1.set_yticklabels([])
+    ax1.set_ylabel(f'{cii_title}')
+    ax[1].xaxis.set_minor_locator(MultipleLocator(10))
+
+    ax[2].plot(mg_wavelength, mg_data_arr[time_x, height], color='k')
+    ax[2].set_ylabel(' ')
+    ax[2].set_xlabel('Wavelength (Å)')
+    ax[2].set_ylim(0, y_lim)
+    ax2 = ax[2].twinx()
+    ax2.set_yticks([])
+    ax2.set_yticklabels([])
+    ax2.set_ylabel(f'{mg_title}')
+    ax[2].xaxis.set_minor_locator(MultipleLocator(10))
+
+    plt.suptitle(f'Time: {time_x} s')
+    save_path = os.path.join(output_loc, f"Wavelength_Profiles_{time_x}s.png")
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.show()
+    plt.close(fig)
 
 line_profile(lines, time_x)
 
