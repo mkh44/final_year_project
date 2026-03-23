9ffc32bd9f8e65ad09ae8e01384c46c30d62862d
mkh44 <molly.hurley24@mail.dcu.ie>
2026-03-23 19:11:57 +0000

    creating sji

--- example_lines.py
+++ example_lines.py
@@ -2,6 +2,7 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.wcs import WCS
+from matplotlib.gridspec import GridSpec
 
 
 def time_to_index(time_array, target_time):
@@ -40,29 +41,59 @@ frame_idx = sji_indices[0]  # or loop through indices
 wcs_2d = wcs.slice([frame_idx, slice(None), slice(None)])
 
 
-fig, axes = plt.subplots(1, len(time_seconds), figsize=(16, 4), subplot_kw={'projection': wcs_2d})
+# fig, axes = plt.subplots(1, len(time_seconds), figsize=(16, 4), subplot_kw={'projection': wcs_2d}, constrained_layout=True)
+#
+# for i, (t, idx) in enumerate(zip(time_seconds, sji_indices)):
+#     img = data[idx, :, :]
+#
+#     vmin = np.percentile(img, 0.5)
+#     vmax = np.percentile(img, 99.5)
+#
+#     axes[i].imshow(img, origin='lower', cmap='magma', vmin=vmin, vmax=vmax)
+#
+#
+#     axes[i].axhline(y_pix, color='k', linestyle='--', linewidth=2)
+#     axes[i].axvline(crpix2 - 9, color='k', linewidth=2)
+#     axes[i].set_xlabel("Solar X (arcsec)")
+#
+#     axes[i].set_title(f"t = {t}s")
+#
+#     if i == 0:
+#         axes[i].set_ylabel("Solar Y (arcsec)")
+#     else:
+#         axes[i].set_ylabel(' ')
+n = len(time_seconds)
+
+
+fig = plt.figure(figsize=(16, 4))
+gs = GridSpec(1, n, figure=fig, wspace=0.01)
 
 for i, (t, idx) in enumerate(zip(time_seconds, sji_indices)):
-    img = data[idx, :, :]
 
+    wcs_2d = wcs.slice([idx, slice(None), slice(None)])
+
+
+    ax = fig.add_subplot(gs[0, i], projection=wcs_2d)
+
+    img = data[idx, :, :]
     vmin = np.percentile(img, 0.5)
     vmax = np.percentile(img, 99.5)
 
-    axes[i].imshow(img, origin='lower', cmap='magma', vmin=vmin, vmax=vmax)
+    im = ax.imshow(img, origin='lower', cmap='magma', vmin=vmin, vmax=vmax)
+    ax.axhline(y_pix, color='black', linestyle='--', linewidth=2)
 
-
-    axes[i].axhline(y_pix, color='k', linestyle='--', linewidth=2)
-    axes[i].axvline(crpix2 - 9, color='k', linewidth=2)
-    axes[i].set_xlabel("Solar X (arcsec")
-
-    axes[i].set_title(f"t = {t}s")
 
     if i == 0:
-        axes[i].set_ylabel("Solar Y (arcsec)")
+        ax[i].set_ylabel("Solar Y (arcsec)")
     else:
         axes[i].set_ylabel(' ')
 
-plt.subplots_adjust(wspace=0.02)
-plt.tight_layout()
+    ax.set_title(f"t = {t}s")
+    ax.set_xlabel("Solar X (arcsec)")
+    ax.set_ylabel("Solar Y (arcsec)")
+
+
+
 plt.show()
+
 
